9899509cc553b6a656f7509feb8841110275a9fb
fizer-li <kevin_fangfang@163.com>
2021-07-14 14:40:14 +0800

    封装异步请求

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App'
 import store from './store'
 Vue.config.productionTip = false
+
+// 引入封装的异步请求
+import request from './utils/request'
+Vue.prototype.request = request
 //把vuex定义为全局组件
 Vue.prototype.$store = store
 App.mpType = 'app'
